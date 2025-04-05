a7f8979fd6cb2ade221fa9a8aecf79613a5e857f
Juliusz Pawlus <60894769+Bladdyy@users.noreply.github.com>
2025-04-05 15:54:33 +0200

    Update Reducer.hs

--- projekt2/app/Reducer.hs
+++ projekt2/app/Reducer.hs
@@ -6,73 +6,59 @@ import Common
 -- Performs one step of reduction.
 rstep :: Maybe Expr -> Maybe Expr -> DefMap -> (Maybe Expr, Maybe Expr)
 rstep pref Nothing _ = (pref, Nothing)
-
-
--- Cofaj po wyrażeniu, aż dojedziesz do końca i zapisuj ilość argumetnów;
-rstep pref (Just (Var x)) mapping = 
-    case Map.lookup x mapping of 
-     Nothing -> case pref of
-                 Just expr -> (Just (expr :$ Var x), Nothing)
-                 Nothing -> (Just (Var x), Nothing)
-     Just (Def _ pats expr) -> if null pats then (pref, Just expr)
-                                  else case pref of
-                                        Just ex -> (Just (ex :$ Var x), Nothing)
-                                        Nothing -> (Just (Var x), Nothing)
-
-
-
-rstep pref (Just (e1 :$ e2)) mapping = let (h, rest) = extractHead (e1 :$ e2) 
-                                            in 
-                                            case Map.lookup h mapping of
-                                              Nothing -> case pref of
-                                                           Nothing -> rstep (Just (Var h)) (rest) mapping
-                                                           Just expr -> rstep (Just (expr :$ Var h)) (rest) mapping
-                                              Just val -> checkstep pref val (e1 :$ e2) mapping
+rstep pref (Just expr) mapping = let (new_pref, rest, _, _, _) = findReduce pref expr 0
+                                   in
+                                   (new_pref, rest)
     where
-    extractHead (Var x) = (x, Nothing)
-    extractHead (e1' :$ e2') = let (h', rest') = extractHead e1'
-                                in
-                                case rest' of
-                                  Nothing -> (h', Just e2')
-                                  Just val' -> (h', Just (val' :$ e2'))
+    -- Finds first reducable part, reduces and passes it. Adds non-reducable part to prefix.
+    findReduce :: Maybe Expr -> Expr -> Int -> (Maybe Expr, Maybe Expr, Def, Int, [Expr])
+    findReduce pref' (e1 :$ e2) n = 
+        let (new_pref, rest, Def nam par ex, to_param, lst) = findReduce pref' e1 (n + 1)
+          in
+          case to_param of
+            -- Looking for reducable part.
+            (-1) -> case e2 of
+                      (e1' :$ e2') -> findReduce new_pref (e1' :$ e2') n
+                      _ -> findReduce new_pref e2 n
+            -- Reducable part found and reduced already.
+            (-2) -> case rest of
+                      Nothing -> (new_pref, Just e2, Def nam par ex, to_param, lst)
+                      Just expr' -> (new_pref, Just (expr' :$ e2), Def nam par ex, to_param, lst)
+            -- Last argument for reduction. 
+            1 -> (pref', Just (reduce ex par (reverse (e2:lst))), Def nam par ex, -2, [])
+            -- Middle argument for reduction.
+            _ -> (new_pref, rest, Def nam par ex, to_param - 1, e2 : lst)
+
+    findReduce pref' (Var name) n = 
+        case Map.lookup name mapping of
+          Nothing -> case pref' of
+               Nothing -> (Just (Var name), Nothing, Def "a" [] (Var "a"), -1, [])
+               Just expr' -> (Just (expr' :$ Var name), Nothing, Def "a" [] (Var "a"), -1, [])
+          
+          Just (Def _ pats expr') -> 
+              -- No arguments needed.
+              if length pats == 0 
+               then (pref', Just (reduce expr' pats []), (Def name pats expr'), -2, [])
+              -- Enough arguments to perform reduction.
+              else if length pats <= n 
+               then (pref', Nothing, (Def name pats expr'), length pats, [])
+              -- Not enough arguments to perform reduction.
+              else case pref' of
+               Nothing -> (Just (Var name), Nothing, Def "a" [] (Var "a"), -1, [])
+               Just expr2 -> (Just (expr2 :$ Var name), Nothing, Def "a" [] (Var "a"), -1, [])
 
 
+    -- Creates submap for reduced expression.
+    reduce :: Expr -> [String] -> [Expr] -> Expr                   
+    reduce expr' def_pats params = subReduce expr' (Map.fromList (zip def_pats params))
 
+
+    -- Performes reduction.
     subReduce :: Expr -> ExprMap -> Expr
     subReduce (a :$ b) small_map = subReduce a small_map :$ subReduce b small_map
     subReduce (Var name) small_map = case Map.lookup name small_map of
                                      Nothing -> Var name
                                      Just val -> val
-
-    insert ex' (Var _) = ex'
-    insert ex' (e1':$ e2') = insert ex' e1' :$ e2'
-
-    reduce :: Maybe Expr -> Expr -> ExprMap -> Maybe Expr -> (Maybe Expr, Maybe Expr)                   
-    reduce prefs expr' small_mapping rest = case rest of
-                                            Nothing -> (prefs, Just (subReduce expr' small_mapping))
-                                            Just expr -> (prefs, Just (insert (subReduce expr' small_mapping) expr))
-
-
-    takeParams :: Int -> Expr -> [Expr] -> ([Expr], Maybe Expr, Bool)
-    
-    takeParams n (e1' :$ e2') lst = takeParams n e1' (e2' : lst) 
-    takeParams n _ lst = if length lst >= n then (take n lst, revert (drop n lst) (Just (Var "start")), True)
-                         else ([], revert lst Nothing, False)
-        where
-            revert (h:ts) rest = case rest of
-                                    Nothing -> revert ts (Just h)
-                                    Just expr -> revert ts (Just (expr :$ h))
-            revert _ rest = rest
-
-    checkstep :: Maybe Expr -> Def -> Expr -> DefMap -> (Maybe Expr, Maybe Expr)
-    checkstep pref' (Def name pats expr) rest mapping' =
-        let (params, new_rest, out) = takeParams (length pats) rest []
-            in
-            if out 
-                then reduce pref' expr (Map.fromList (zip pats params)) new_rest
-                else case pref of
-                       Nothing -> rstep (Just (Var name)) new_rest mapping'
-                       Just p  -> rstep (Just (p :$ (Var name))) new_rest mapping'
 
 
 
@@ -80,12 +66,12 @@ rstep pref (Just (e1 :$ e2)) mapping = let (h, rest) = extractHead (e1 :$ e2)
 iterateSteps :: Int -> Maybe Expr -> Maybe Expr -> DefMap -> IO ()
 iterateSteps 0 _ _ _ = return ()  -- Stop after n steps
 iterateSteps n pref rest mapping = do
-    let (newPref, newX) = rstep pref rest mapping
-    case newX of 
+    let (new_pref, new_x) = rstep pref rest mapping
+    case new_x of 
         Nothing -> return ()
         Just x -> do
-                  print (buildExpr newPref x)
-                  iterateSteps (n - 1) newPref newX mapping
+                  print (buildExpr new_pref x)
+                  iterateSteps (n - 1) new_pref new_x mapping
     where
         -- Adds prefix before the rest of expression.
         buildExpr :: Maybe Expr -> Expr -> Expr
